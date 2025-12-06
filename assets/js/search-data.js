076c8d317c50ddfcbb30ef856ae9a58a62ab2770
maximilianaberthold <maximilian_berthold@web.de>
2025-12-06 09:50:56 +0000

    Deploying to gh-pages from @ maximilianaberthold/maximilianaberthold.github.io@3adf854071a6ab705022488a50598c1333fef97a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-presentations",
+          title: "presentations",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/presentations/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "",
